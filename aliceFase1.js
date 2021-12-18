e7ce7f7dabca79ec95c219a058d3c1d0a78ce665
Maysa Pereira <maysalvespereira@gmail.com>
2021-12-18 18:58:29 -0300

    correção na página

--- aliceFase1.js
+++ aliceFase1.js
@@ -9,7 +9,7 @@ function escolha (){
     if(decida == 1){
         return location = './aliceFase2-1.html'
     } else if(decida == 2){
-        return location = '../aliceFase2-2.html'
+        return location = './aliceFase2-2.html'
     } else {
         alert('Opção Inválida')
         return escolha();
